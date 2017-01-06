17346ec99144a466d4b9b9ff01447a917109d662
Florian Boucault <florian@boucault.net>
2017-01-06 09:37:18 +0100

    Fixed copy/paste actions availability in contextual menu.

--- src/plugin/qmltermwidget/lib/TerminalDisplay.h
+++ src/plugin/qmltermwidget/lib/TerminalDisplay.h
@@ -489,6 +489,12 @@ public slots:
      */
     void pasteSelection();
 
+    /** Checks if the clipboard is empty */
+    bool isClipboardEmpty();
+
+    /** Checks if the selection is empty */
+    bool isSelectionEmpty();
+
     /** 
        * Changes whether the flow control warning box should be shown when the flow control
        * stop key (Ctrl+S) are pressed.
